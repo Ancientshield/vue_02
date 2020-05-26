961216b157892bbb2936eb6af0b5ae7f09c60bd3
Ancientsword <jacklin2100@gmail.com>
2020-05-26 20:27:54 +0800

    add robot.vue

---
+++ src/robot/robot.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <label>Please ask a yes or no question. For example: Is the sky blue?</label>
+    <input v-model="question" />
+    <button>Search</button>
+    <p>{{ answer }}</p>
+  </div>
+</template>
